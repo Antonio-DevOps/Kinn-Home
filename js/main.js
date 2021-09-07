2a36dff96355177e6f2ee92dbe1af990f067005c
ksion4ik1 <ksion4ik.life@gmail.com>
2021-09-08 01:56:04 +0300

    fix slider for screens

--- js/main.js
+++ js/main.js
@@ -13,11 +13,38 @@ function opentab(evt, cityName) {
   }
   // =====================Slider===================
   $('.sliderNewsforTravel').slick({
-    slidesToShow: 6,
+    slidesToShow: 5,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
     arrows:false,
     centerPadding: true,
-    infinite: true,
+    responsive: [
+      {
+        breakpoint: 1920,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 1170,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
+  
